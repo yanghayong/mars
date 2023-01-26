3fa56682e1f3c4fb92829549312794c58608693f
yanghayong <hayong5621@daum.net>
2023-01-26 11:56:07 +0900

    mars

--- js/common.js
+++ js/common.js
@@ -1,4 +1,13 @@
 $(function () {
-  $('.slide_01').slick()
-  $('.slide_02').slick()
+  $('.slide_01').slick({
+    autoplay: true,
+    nextArrow: $('.next'),
+    prevArrow: $('.prev'),
+  })
+
+  $('.slide_02').slick({
+    autoplay: true,
+    dots: true,
+    fade: true,
+  })
 })
